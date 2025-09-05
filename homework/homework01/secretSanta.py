6406ba342821876eb20a2fa1c1af9fbf6fbd3a3a
Simon <ramsey2005s@gmail.com>
2025-09-05 10:54:09 -0400

    add probabilities notebook

--- homework/homework01/secretSanta.py
+++ homework/homework01/secretSanta.py
@@ -2,9 +2,13 @@
 # CS315 - Algorithms and Analysis
 # Homework 1 - Secret Santa, Multiplication without operator, Multiplication Recursively
 
-from itertools import permutations
+# Prompted ChatGPT to asssist with the initial understanding of the problem and what the correct path was.
+# I specify "no code in this thread" and "only discuss at a high level, don't describe full implementation"
+# GPT directed me towards using a graph-based approach to model the problem, I did some research and landed on this solution.
+
+# Resources used:
+# https://www.geeksforgeeks.org/dsa/ford-fulkerson-algorithm-for-maximum-flow-problem/
+# https://www.geeksforgeeks.org/dsa/maximum-bipartite-matching/
 
 def secret_santa(participants: list, invalid_combinations: list[tuple]) -> None:
-  for p in permutations(participants):
-    if p not in invalid_combinations:
-      print(p)
+  pass
